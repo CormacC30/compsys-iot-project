ec81e4a455b35ef4bf61e4879533713a94f96485
Cormac Costello <117513353+CormacC30@users.noreply.github.com>
2023-12-30 18:09:36 +0000

    working chart now implemented

--- controllers/data-controller.js
+++ controllers/data-controller.js
@@ -1,17 +1,25 @@
 // data-controller.js
 
 import { fetchHistoricalData } from '../models/firebase.js';
+import { analytics } from '../utils/analytics.js';
 
 export const getHistoricalData = async (req, res) => {
   try {
     // Fetch historical data from the database
     const historicalData = await fetchHistoricalData();
-
+    
     // Log the historical data to the console
     console.log('Historical Data:', historicalData);
 
     // Respond with a success message or other appropriate response
-   // res.status(200).json(historicalData);
+    const chartData = await analytics.chartData(historicalData);
+    console.log("chart data: ", chartData);
+    const viewData = {
+      title: "data-controller",
+      chartData: chartData,
+    };
+    res.render("historical-data", viewData);
+    
   } catch (error) {
     console.error('Error fetching historical data:', error);
     res.status(500).send('Internal Server Error');
--- utils/analytics.js
+++ utils/analytics.js
@@ -1,5 +1,21 @@
-import handlebars from "handlebars";
+//import { fetchHistoricalData } from "../models/firebase";
 
 export const analytics = {
+
+async chartData(chartData){
+    
+    let report = {
+        tempTrend: [],
+        trendLabels: []
+      };
+      const startIndex = Math.max(0, chartData.length - 100);
+    const endIndex = chartData.length;
+    for (let i = startIndex; i < endIndex; i++){
+        const timestamp = new Date(chartData[i].timestamp).getTime() / 1000;  // Convert to Unix timestamp
+        report.tempTrend.push(chartData[i].temperature);
+        report.trendLabels.push(timestamp);
+    }
+    return report;
+}
 
 };
